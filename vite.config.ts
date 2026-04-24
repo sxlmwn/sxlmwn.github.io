600abc0ec1639fbfd82730fd525179cf6680a994
Muhammad Salman Younus <salmanyounus13@gmail.com>
2026-04-25 01:40:44 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
+export default defineConfig(({ mode }) => ({  server: {
+    base: '/',
     host: "::",
     port: 8080,
     hmr: {
